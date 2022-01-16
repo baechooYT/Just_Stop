f789091220513ab7617d822fba03202078d268db
beachoo_YT <66154521+baechooYT@users.noreply.github.com>
2022-01-16 21:24:38 +0900

    Create funi2.lua

---
+++ funi2.lua
@@ -0,0 +1,33 @@
+--[[
+
+███████╗██╗░░░██╗███╗░░██╗██╗██████╗░
+██╔════╝██║░░░██║████╗░██║██║╚════██╗
+█████╗░░██║░░░██║██╔██╗██║██║░░███╔═╝
+██╔══╝░░██║░░░██║██║╚████║██║██╔══╝░░
+██║░░░░░╚██████╔╝██║░╚███║██║███████╗
+╚═╝░░░░░░╚═════╝░╚═╝░░╚══╝╚═╝╚══════╝                                                                           why are you here
+]]--
+
+local funi2_msg = "\n Kicked For Exploiting \n Raeson : Spamming Keys So Fast Like Bot"
+
+if not iswindowactive or not keypress or not keyrelease then
+     game.Players.LocalPlayer:Kick("\n Your Exploit Not Surpport This Script.")
+end
+
+spawn(function()
+    while iswindowactive() == false do wait() end
+    wait(0.5)
+    game.Players.LocalPlayer:Kick(funi2_msg)
+end)
+
+
+while wait() do
+    spawn(function()
+        while wait() do 
+            spawn(function()
+                keypress(0x7A)
+                keyrelease(0x7A)
+            end)
+        end
+    end)
+end
